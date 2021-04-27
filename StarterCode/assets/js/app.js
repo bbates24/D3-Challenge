a26da4be8eb0179f80e411e3c78872eba95ded2a
brian bates <batesbj24@gmail.com>
2021-04-27 19:50:56 -0400

    final commit

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,119 +1,66 @@
 // @TODO: YOUR CODE HERE!
-// Create a Scatter Plot 
+// Setting the variables 
+var xaxis= poverty
+var yaxis= healthcare
+
+// Updating the X scale and Variable
+var xScale = d3.scaleBand()
+  .domain(var yScale = d3.scaleLinear()
+  .domain([0, 100])
+  .range([0, svgHeight]);)
+ 
+
+  //Updating the Y scale and Variable
+  var yScale = d3.scaleLinear()
+  .domain([0, 100])
+  .range([0, svgHeight]);
+
+  // Updating Axis functions 
+  var bottomAxis = d3.axisbottom(xLinearScale)
+  var leftAxis = d3.axisleft(ylinearScale)
+
+  // Append Axes to Charts 
+  var chartGroup = svg.append("g")
+  .attr("transform", `translate(${svgHeight}, ${margin.top})`);
+  .call(bottomAxis)
+
+  var chartGroup = svg.append("g")
+  .call(leftAxis)
+  
+// Define SVG area dimensions
 var svgWidth = 960;
-var svgHeight = 700;
+var svgHeight = 500;
 
+// Define the chart's margins as an object
 var margin = {
-  top: 20,
-  right: 40,
+  top: 60,
+  right: 60,
   bottom: 60,
-  left: 100
+  left: 60
 };
+// Define dimensions of the chart area
+var chartWidth = svgWidth - margin.left - margin.right;
+var chartHeight = svgHeight - margin.top - margin.bottom;
 
-var width = svgWidth - margin.left - margin.right;
-var height = svgHeight - margin.top - margin.bottom;
-
-// Create a SVG wrapper, append a SVG Chart for the group
-var svg = d3.select("#scatter")
+// Select body, append SVG area to it, and set its dimensions
+var svg = d3.select("body")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
 
-var chartGroup = svg.append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
-d3.csv("assets/data/data.csv").then(function (stateData) {
-  console.log(stateData);
-
-  // Parse Data 
-  stateData.forEach(function (data) {
+  // Load data from data.csv
+d3.csv("data.csv").then(function(data) {
+   // Format the data as numbers
+   stateData.forEach(function(data) {
     data.poverty = +data.poverty;
     data.healthcare = +data.healthcare;
-});
-
-// Create Scale
-var xLinearScale = d3.scaleLinear()
-    .domain([0, d3.max(stateData, d => d.poverty)])
-    .range([0, width]);
-
-  var yLinearScale = d3.scaleLinear()
-    .domain([0, d3.max(stateData, d => d.healthcare)])
-    .range([height, 0]);
-
-// Create Functions
-var xLinearScale = d3.scaleLinear()
-    .domain([0, d3.max(stateData, d => d.poverty)])
-    .range([0, width]);
-
-  var yLinearScale = d3.scaleLinear()
-    .domain([0, d3.max(stateData, d => d.healthcare)])
-    .range([height, 0]);
-
-
-// Create Axis
-var bottomAxis = d3.axisBottom(xLinearScale);
-  var leftAxis = d3.axisLeft(yLinearScale);
-
-// Append the Axis to chart
-chartGroup.append("g")
-.attr("transform", `translate(0, ${height})`)
-.call(bottomAxis);
-
-chartGroup.append("g")
-.call(leftAxis);
-
-// Create Circles
-var circlesGroup = chartGroup.selectAll("circle")
-    .data(stateData)
-    .enter()
-    .append("circle")
-    .attr("cx", d => xLinearScale(d.poverty))
-    .attr("cy", d => yLinearScale(d.healthcare))
-
-    .attr("r", "12")
-    
-    .attr("fill", "blue")
-
-    .attr("opacity", ".5");  
-
-// Create the tool tip
-var toolTip = d3.tip()
-.attr("class", "tooltip")
-.offset([80, -60])
-.html(function (d) {
-  return (`${d.state}<br>Poverty: ${d.poverty}<br>Healthcare: ${d.healthcare}`);
-});
-
-// Create Tooltip in chart
-chartGroup.call(toolTip);
-
-// Listeners for Tooltip
-circlesGroup.on("click", function (data) {
-    toolTip.show(data, this);
-
-      on("mouseout", function (data, index) {
-        toolTip.hide(data);
-      });
-
-// Create Axis Labels
-chartGroup.append("text") 
-    .attr("transform", "rotate(-90)")
-    .attr("y", 0 - margin.left + 40)
-    .attr("x", 0 - (height / 2))
-    .attr("dy", "1em")
-    .attr("class", "axisText")
-    .style("fill", "black")
-    .style("font", "20px sans-serif")
-    .style("font-weight", "bold")
-    .text("People without Healthcare (%)");
-
-  chartGroup.append("text")
-    .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
-    .attr("class", "axisText")
-    .style("font", "20px sans-serif")
-    .style("font-weight", "bold")
-    .text("Poverty (%)");
-
-  }).catch(function (error) {
+  });
+  // Append an SVG group element to the SVG area, create the bottom axis inside of it
+  // Translate the bottom axis to the bottom of the page
+  chartGroup.append("g")
+    .classed("axis", true)
+    .attr("transform", "translate(0, " + chartHeight + ")")
+    .call(bottomAxis);
+}).catch(function(error) {
   console.log(error);
 });
